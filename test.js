34c48d4dd1d33622f4a9ddec7c0f850e21093577
lenpio0 <127386860+lenpio0@users.noreply.github.com>
2023-03-26 21:29:50 +0200

    Revert "V1.1"
    This reverts commit 07f1e17d0c977626df7c8a86c10d36c11ab8de6b.

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-// Get HTML elements into objects
-
 let rps = document.getElementById('rps');
 let r = document.getElementById('r');
 let p = document.getElementById('p');
@@ -12,11 +10,17 @@ let bo1 = document.getElementById('bo1');
 let bo3 = document.getElementById('bo3');
 let bo5 = document.getElementById('bo5');
 let bo9 = document.getElementById('bo9');
+let reward = document.getElementById('reward');
 let yourScore = 0;
 let hisScore = 0;
 let win = 0;
-
-// Give thoses elements a behaviour on click
+let rick = document.getElementById('rick');
+let rickGif = document.getElementById('rick-gif');
+let stopIt = document.getElementById('stop-it');
+let youDied = document.getElementById('you-died');
+const rickRoll = new Audio(
+    "never-gonna.mp3"
+  );  
 
 r.onclick = rock;
 p.onclick = paper;
@@ -27,8 +31,7 @@ bo1.onclick = BO1;
 bo3.onclick = BO3;
 bo5.onclick = BO5;
 bo9.onclick = BO9;
-
-// BO buttons will define the number of wins needed and replace themselves with rock-paper-scissors buttons
+stopIt.onclick = plzStop;
 
 function BO1() {
     rps.innerHTML = 'Pierre-Papier-Ciseaux !'
@@ -78,28 +81,26 @@ function BO9() {
     win = 5;
 }
 
-// rival will randomly define the "opponent" choice, which will determine which text appears and score incrementation if there is one. it 
-// will then check if someone is winning, and end the game if needed.
-// 0 rock
-// 1 paper
-// 2 scissors
-
 function rock() {
     let rival = Math.floor(Math.random() * 3);
-    r.classList.add('hidden');
-    p.classList.add('hidden');
-    s.classList.add('hidden');
-    ok.classList.remove('hidden'); 
     if (rival == 0) {
-        rps.innerHTML = 'Pierre !</br>Rejouez :O';
+        rps.innerHTML = 'Pierre !</br>Rejouez';
     }
     else if (rival == 1) {
         rps.innerHTML = 'Papier !</br>Point pour moi >:)';
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
         hisScore ++;
         hisScoreP.innerHTML = 'Score Adverse : ' + hisScore;
     }
     else if (rival == 2) {
         rps.innerHTML = ' Ciseaux !</br>Point pour toi \\o/';
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
         yourScore ++;
         yourScoreP.innerHTML = 'Votre Score : ' + yourScore;
     }
@@ -119,7 +120,16 @@ function rock() {
                 "Erreur dans le switch :'(";
                 break;
         }
-        rps.innerHTML = result + " !</br>gg t'as gagné !";
+        rps.innerHTML = result + " !</br>gg t'as gagné t'es trop un bg (ou une bg hein je suis un jeu progressiste)";
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
+        reward.classList.remove('hidden');
+        rickGif.classList.remove('hidden');
+        stopIt.classList.remove('hidden');
+          rickRoll.play();
+
     }
     if (hisScore == win) {
         let result = 0
@@ -137,26 +147,35 @@ function rock() {
                 "Erreur dans le switch :'(";
                 break;
         }
-        rps.innerHTML = result + " !</br>t'as perdu déso pas déso";
+        rps.innerHTML = result + " !</br>Booouh t'as perdu cheh";
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
+        youDied.classList.remove('hidden');
     }
 }
 
 function paper() {
     let rival = Math.floor(Math.random() * 3);
-    r.classList.add('hidden');
-    p.classList.add('hidden');
-    s.classList.add('hidden');
-    ok.classList.remove('hidden');
     if (rival == 0) {
         rps.innerHTML = 'Pierre !</br>Point pour toi \\o/';
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
         yourScore ++;
         yourScoreP.innerHTML = 'Votre Score : ' + yourScore;
     }
     else if (rival == 1) {
-        rps.innerHTML = 'Papier !</br>Rejouez :O';
+        rps.innerHTML = 'Papier !</br>Rejouez';
     }
     else if (rival == 2) {
         rps.innerHTML = 'Ciseaux !</br>Point pour moi >:)';
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
         hisScore ++;
         hisScoreP.innerHTML = 'Score Adverse : ' + hisScore;
     }
@@ -176,7 +195,16 @@ function paper() {
                 "Erreur dans le switch :'(";
                 break;
         }
-        rps.innerHTML = result + " !</br>gg t'as gagné !";
+        rps.innerHTML = result + " !</br>gg t'as gagné t'es trop un bg (ou une bg hein je suis un jeu progressiste)";
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
+        reward.classList.remove('hidden');
+        rickGif.classList.remove('hidden');
+        stopIt.classList.remove('hidden');
+      
+          rickRoll.play();
     }
     if (hisScore == win) {
         let result = 0
@@ -194,28 +222,37 @@ function paper() {
                 "Erreur dans le switch :'(";
                 break;
         }
-        rps.innerHTML = result + " !</br>t'as perdu déso pas déso";
+        rps.innerHTML = result + " !</br>Booouh t'as perdu cheh";
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
+        youDied.classList.remove('hidden');
     }
 }
 
 function scissors() {
     let rival = Math.floor(Math.random() * 3);
-    r.classList.add('hidden');
-    p.classList.add('hidden');
-    s.classList.add('hidden');
-    ok.classList.remove('hidden');
     if (rival == 0) {
         rps.innerHTML = 'Pierre !</br>Point pour moi >:)';
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
         hisScore ++;
         hisScoreP.innerHTML = 'Score Adverse : ' + hisScore;
     }
     else if (rival == 1) {
         rps.innerHTML = 'Papier !</br>Point pour toi \\o/';
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
         yourScore ++;
         yourScoreP.innerHTML = 'Votre Score : ' + yourScore;
     }
     else if (rival == 2) {
-        rps.innerHTML = 'Ciseaux !</br>Rejouez :O';
+        rps.innerHTML = 'Ciseaux !</br>Rejouez';
     }
     if (yourScore == win) {
         let result = 0
@@ -233,7 +270,15 @@ function scissors() {
                 "Erreur dans le switch :'(";
                 break;
         }
-        rps.innerHTML = result + " !</br>gg t'as gagné !";
+        rps.innerHTML = result + " !</br>gg t'as gagné t'es trop un bg (ou une bg hein je suis un jeu progressiste)";
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
+        reward.classList.remove('hidden');
+        rickGif.classList.remove('hidden');
+        stopIt.classList.remove('hidden');
+          rickRoll.play();
     }
     if (hisScore == win) {
         let result = 0
@@ -251,12 +296,18 @@ function scissors() {
                 "Erreur dans le switch :'(";
                 break;
         }
-        rps.innerHTML = result + " !</br>t'as perdu déso pas déso";
+        rps.innerHTML = result + " !</br>Booouh t'as perdu cheh";
+        r.classList.add('hidden');
+        p.classList.add('hidden');
+        s.classList.add('hidden');
+        ok.classList.remove('hidden');
+        youDied.classList.remove('hidden');
     }
 }
 
 function okay() {
     ok.classList.add('hidden');
+    youDied.classList.add('hidden');
     if (yourScore == win || hisScore == win) {
         replay.classList.remove('hidden');
         rps.innerHTML = 'Rejouer ?';
@@ -275,6 +326,7 @@ function again() {
     bo3.classList.remove('hidden');
     bo5.classList.remove('hidden');
     bo9.classList.remove('hidden');
+    reward.classList.add('hidden');
     rps.innerHTML = 'Pierre-papier-ciseaux !</br>Combien de manches gagnantes ?';
     win = 0;
     yourScore = 0;
@@ -284,3 +336,13 @@ function again() {
 
 }
 
+function plzStop() {
+    rickGif.classList.add('hidden');
+    stopIt.classList.add('hidden');
+    reward.classList.add('hidden');
+    rickRoll.pause();
+    rickRoll.currentTime = 0;
+}
+// 0 rock
+// 1 paper
+// 2 scissors
